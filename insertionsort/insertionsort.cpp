73d670ec9ee346ff96e77ada35394e3c06fc71b2
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-03-23 17:57:28 +0700

    tambah fungsi void insertion

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -38,7 +38,24 @@ void insertionsort()
     int temp; // membuat variable data temporer atau penyimpanan sementara
     int j;    // membuat variable j sebagai penanda
 
+    for (int i = 1; i < n; i++) // 1. Looping dengan i dimulai dari 1 hingga n-1
+    {
+        temp = arr[i]; // 2. simpan nilai arr[i] ke variable sementara temp
 
+        j = i - 1;  // 3. setting nilai j sama dengan i-1;
+
+        while (j >= 0 && arr[j > temp]) // 4. looping while dimana nilai j lebih besar sama dengan
+        {
+            // arr[j] lebih besar sama dengan temp
+            arr[j + 1] = arr[j]         //a. simpan arr [j] ke dalam variable arr[j + 1]
+            j-;                         //b. decrement nilai j by `1
+
+        }
+        arr[j + 1] = temp;              //5. simpan nilai temp ke dalam arr[j + 1]
+
+
+
+    }
 }
 
 int main()
