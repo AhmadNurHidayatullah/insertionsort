96771317ef16b846954fe7dea7ad149cf140ea35
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-03-23 18:04:46 +0700

    tambah fungsi void display

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -71,7 +71,17 @@ void display()
     cout << "\n==================" << endl;      // output ke layar
     cout << "element array yang telah tersusun" << endl; // output ke layar
     cout << "===================" << endl; // output ke layar
+
+    for (int j = 0; j < n; j++)
+    {                           // looping dengan j dimulai dari 0 hingga n-1
+        cout << arr[j] << endl; // output ke layar
+
+
+    }
+    cout << endl;   // output baris kosong
+
 }
+
 int main()
 {
     std::cout << "Hello World!\n";
